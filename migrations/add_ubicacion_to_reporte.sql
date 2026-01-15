90a1a95262837344a759972a6ce4a7a50db77892
dianita77727@hotmail.com <dbulla.marinp@gmail.com>
2026-01-15 16:53:49 -0500

    Migrations: Descomenta UPDATE para poblar ubicaciones en reportes históricos

--- migrations/add_ubicacion_to_reporte.sql
+++ migrations/add_ubicacion_to_reporte.sql
@@ -4,7 +4,7 @@ ALTER TABLE reporte ADD COLUMN ubicacion VARCHAR(50) DEFAULT 'exterior' COMMENT 
 -- Crear índice para búsquedas por ubicación (opcional)
 CREATE INDEX idx_reporte_ubicacion ON reporte(ubicacion);
 
--- Actualizar reportes existentes basándose en la ubicación de su sensor (opcional)
--- UPDATE reporte r 
--- INNER JOIN sensor s ON r.codigo_sensor = s.codigo 
--- SET r.ubicacion = s.ubicacion;
+-- Actualizar reportes existentes basándose en la ubicación de su sensor
+UPDATE reporte r 
+INNER JOIN sensor s ON r.codigo_sensor = s.codigo 
+SET r.ubicacion = s.ubicacion;
